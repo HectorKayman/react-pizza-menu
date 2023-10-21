94e8e7adc6936e2a730d01630fed2792b0fdb147
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-10-21 05:36:40 +0530

    Add JSX logic inside a component as a demo

--- src/index.js
+++ src/index.js
@@ -72,6 +72,14 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  if (isOpen) alert('We are currently open');
+  else alert("Sorry we're closed");
+
   return (
     <footer>{new Date().toLocaleTimeString()}We are currently open!</footer>
   );
