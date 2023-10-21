91ff697f636a3ce1dc6055d84edd5e0f096342a0
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-10-21 05:30:11 +0530

    Render all the required components

--- src/index.js
+++ src/index.js
@@ -49,7 +49,21 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
@@ -57,16 +71,10 @@ function App() {
   );
 }
 
-function Header() {
-  //
-}
-
-function Menu() {
-  //
-}
-
 function Footer() {
-  //
+  return (
+    <footer>{new Date().toLocaleTimeString()}We are currently open!</footer>
+  );
 }
 
 function Pizza() {
